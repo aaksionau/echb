7942e7da6aa09a3523e453871f65ea5598632f5d
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-09-11 15:22:37 +0300

    mobile css

--- echb/pages/views.py
+++ echb/pages/views.py
@@ -41,7 +41,7 @@ class HomePageView(View):
         news = NewsItem.objects.all().order_by('-publication_date')[:6]
         articles = Article.objects.all().order_by('-date').select_related('author').select_related('category')[:6]
         events = Event.objects.all().order_by('date')[:3]
-        photos = Gallery.objects.all().order_by('date')[:3]
+        photos = Gallery.objects.all().order_by('-date')[:4]
         form = SubscriberForm()
         context = {
             'page': page,
