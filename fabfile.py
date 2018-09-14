a084e5f288c10fa468c1e89aa67402381a68544f
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-09-14 17:38:28 +0300

    deleted ie.css

--- fabfile.py
+++ fabfile.py
@@ -3,25 +3,6 @@ from fabric.operations import prompt, put
 from fabric.context_managers import prefix
 from fabric.contrib.console import confirm
 
-@task
-def prepare():
-    test()
-    commit()
-    push()
-
-def test():
-    with settings(warn_only=True):
-        result = local("python manage.py test --settings=echb.settings.test")
-    if result.failed and not confirm("Tests failed. Continue?"):
-        abort("Aborted at user request.")
-
-def push():
-    local("git push origin master")
-
-
-def commit():
-    message = prompt("Enter a git commit message: ")
-    local('git add . && git commit -am "{}"'.format(message))
 
 env.use_ssh_config = True
 env.hosts = ["webfaction"]
@@ -29,13 +10,33 @@ env.remote_app_dir = '/home/paloni/webapps/echb_project/echb/'
 env.remote_app_static_dir = '/home/paloni/webapps/echb_static/'
 env.remote_apache_dir = '/home/paloni/webapps/echb_project/apache2/'
 
-@task
-def restart_server():
-    run(f'{env.remote_apache_dir}/bin/restart')
-
+test_results = False
 
 @task
 def deploy():
+    test()
+    commit()
+    push()
+    if test_results:
+        deploy_to_server()
+
+def test():
+    with settings(warn_only=True):
+        result = local("python manage.py test --settings=echb.settings.test")
+        if result.failed and not confirm("Tests failed. Continue?"):
+            abort("Aborted at user request.")
+        else:
+            test_results = True
+    
+def push():
+    local("git push origin master")
+
+def commit():
+    message = prompt("Enter a git commit message: ")
+    local('git add . && git commit -am "{}"'.format(message))
+
+
+def deploy_to_server():
     with cd(f'{env.remote_app_dir}'):
         run('git pull origin master')
 
