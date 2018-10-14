6a3d88d4c17ee31332d8b30139c02ab1972f30d3
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-10-14 04:50:49 +0300

    admin menu changes

--- echb/accounts/views.py
+++ echb/accounts/views.py
@@ -14,6 +14,7 @@ from pages.models import OldUser, Video
 from .models import PrayerRequest
 from .forms import SignUpForm
 
+
 class SignUpFormView(FormView):
     template_name = 'accounts/signup.html'
     form_class = SignUpForm
@@ -24,14 +25,17 @@ class SignUpFormView(FormView):
         auth_login(self.request, user)
         return super(SignUpFormView, self).form_valid(form)
 
+
 class LoginUser(LoginView):
     template_name = 'accounts/login.html'
 
+
 class ProfileUserFormView(UpdateView):
-    template_name='accounts/profile.html'
+    template_name = 'accounts/profile.html'
     model = User
     fields = ['first_name', 'last_name', 'email']
     success_url = '/accounts/profile/success/'
+
 
 class MyPrayerRequestListView(ListView):
     model = PrayerRequest
@@ -40,8 +44,10 @@ class MyPrayerRequestListView(ListView):
 
     def get_queryset(self):
         queryset = super(MyPrayerRequestListView, self).get_queryset()
-        queryset = PrayerRequest.objects.filter(user=self.request.user).select_related('user')
+        queryset = PrayerRequest.objects.filter(
+            user=self.request.user).select_related('user')
         return queryset
+
 
 class PrayerRequestsView(LoginRequiredMixin, ListView):
     model = PrayerRequest
@@ -49,25 +55,26 @@ class PrayerRequestsView(LoginRequiredMixin, ListView):
     template_name = 'accounts/prayerrequest_list.html'
 
     def get_prayer_requests(self):
-        date_delta = timezone.now() -  timedelta(days=6)
-        prayer_requests_all = PrayerRequest.objects.filter(created__gte = date_delta).select_related('user').order_by('created')
+        date_delta = timezone.now() - timedelta(days=6)
+        prayer_requests_all = PrayerRequest.objects.filter(
+            created__gte=date_delta).select_related('user').order_by('created')
 
         return date_delta, prayer_requests_all
-        
+
     def get_context_data(self, **kwargs):
         context = super(PrayerRequestsView, self).get_context_data(**kwargs)
         date_delta, prayer_requests_all = self.get_prayer_requests()
         context['last_video_date'] = date_delta
         context['prayer_requests_all'] = prayer_requests_all
-        context['video'] = Video.objects.filter(category__slug = 'preobrazhenie').select_related('category').order_by('date').first()
+        context['video'] = Video.objects.filter(
+            category__slug='preobrazhenie').select_related('category').order_by('date').first()
 
         return context
 
     def render_to_response(self, context):
         if self.request.is_ajax():
-            data = self.get_context_data()["prayer_requests_all"].values('user', 'description', 'created', 'user__first_name')
+            data = self.get_context_data()["prayer_requests_all"].values(
+                'user', 'description', 'created', 'user__first_name')
             return JsonResponse(list(data), safe=False)
         else:
-            return ListView.render_to_response(self,context)
-            
-
+            return ListView.render_to_response(self, context)
