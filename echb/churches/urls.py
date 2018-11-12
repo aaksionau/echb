ac214e08ea9dbaf0a48a63130d3fd985627e1476
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-11-12 07:54:56 +0200

    deleted geolocation

--- echb/churches/urls.py
+++ echb/churches/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.find_church, name='find-church'),
     path('churches.js', views.get_churches, name='churches'),
-    path('regions.js', views.get_regions, name='regions'),
-    path('geolocation/', views.geolocation, name='geolocation')
+    path('regions.js', views.get_regions, name='regions')
 ]
--- echb/churches/views.py
+++ echb/churches/views.py
@@ -1,7 +1,4 @@
-import json
 import logging
-import urllib.request
-from urllib.parse import quote
 
 from django.core import serializers
 from django.shortcuts import HttpResponse, render
@@ -13,28 +10,6 @@ logger = logging.getLogger('ECHB')
 
 def find_church(request):
     return render(request, 'churches/churches.html')
-
-
-google_key = 'AIzaSyAP8zWTk8oq_9o_OXwSWvkBhZ65163UXhA'
-google_geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
-
-
-def geolocation(request):
-    churches = Church.objects.all()
-
-    for church in churches:
-        address = quote(church.region.name + ' ' + church.address)
-
-    try:
-        with urllib.request.urlopen(f"{google_geocode_url}?address={address}&key={google_key}") as url:
-            data = json.loads(url.read().decode('utf-8'), encoding='utf-8')
-
-            church.lat = data['results'][0]['geometry']['location']['lat']
-            church.lng = data['results'][0]['geometry']['location']['lng']
-            # church.save()
-    except Exception as ex:
-        logger.info(f'Church coordinates wasnt found: {church.title}')
-        logger.error(ex.args)
 
 
 def get_churches(request):
