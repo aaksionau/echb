57025560296bf179296e3c5690f851730e14fcb7
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-12-03 09:29:18 +0200

    autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+// Less configuration
+const gulp = require("gulp"),
+  browserSync = require("browser-sync").create(),
+  sass = require("gulp-sass"),
+  cleanCSS = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  minify = require("gulp-minify"),
+  minifyCss = require('gulp-minify-css'),
+  concat = require("gulp-concat"),
+  autoprefixer = require('gulp-autoprefixer');
+
+const exec = require('child_process').exec;
+
+const pathToStatic = './echb/static/'
+
+sass.compiler = require("node-sass");
+
+gulp.task("server", function() {});
+
+gulp.task("make-map", () => {
+  return gulp
+    .src(`{pathToStatic}js/map/*.js`)
+    .pipe(concat("map.js"))
+    .pipe(minify())
+    .pipe(gulp.dest(`{pathToStatic}js`));
+});
+
+gulp.task("make-css", () => {
+  return gulp
+    .src("./echb/static/css/style.sass")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(minifyCss())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./echb/static/css"));
+});
+
+gulp.task('runserver', function() {
+  var proc = exec('fab runserver')
+})
+
+gulp.task('browserSync', ['runserver'], function() {
+  browserSync.init({
+    notify: false,
+    port: 8000,
+    proxy: 'localhost:8000'
+  })
+});
+
+gulp.task("default", ['make-css', 'make-map', 'browserSync'], function() {
+  gulp.watch("./echb/static/css/**/**/*.sass", ["make-css"]);
+  gulp.watch("./echb/static/css/style.min.css").on('change', browserSync.reload);
+  gulp.watch(`{pathToStatic}js/map/*.js`, ["make-map", "browserSync"]);
+});
