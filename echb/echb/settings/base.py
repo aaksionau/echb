aaa9851a6a31f1b079dcd5c0b6f800cd503ea8b1
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2018-12-05 08:11:13 +0200

    html minifier

--- echb/echb/settings/base.py
+++ echb/echb/settings/base.py
@@ -42,6 +42,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
 
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
