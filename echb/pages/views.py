2915b3bb98fe0906152b52f6969b37a112bcd553
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-04-30 11:26:12 +0300

    update filter for news

--- echb/pages/views.py
+++ echb/pages/views.py
@@ -44,7 +44,7 @@ class HomePageView(View):
         page = Page.objects.get(slug='home')
         news = NewsItem.objects.all().prefetch_related('author').order_by('-publication_date')[:6]
         articles = Article.objects.all().order_by('-date').prefetch_related('author').select_related('category')[:6]
-        events = Event.objects.filter(date__gt=datetime.now()).order_by('date')[:6]
+        events = Event.objects.filter(date__gt=datetime.now(), date__lt=datetime.now()).order_by('date')[:6]
         photos = Gallery.objects.all().prefetch_related('author').order_by('-date')[:4]
         videos = Video.objects.filter(interesting_event=True).order_by('-date')[:4]
         form = SubscriberForm()
